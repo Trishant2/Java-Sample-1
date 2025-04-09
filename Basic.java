2f4ddb8a1a0c567ea6da343940f468302a86c558
satyam kewat <satyam.cs231189@global.orh.in>
2025-04-09 13:52:25 +0530

    heelo

---
+++ Basic.java
@@ -0,0 +1,25 @@
+
+public class Basic {
+    public static void main(String[] arg){
+        int a =3 ;
+        switch (a){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Friday");
+            break;
+            case 5:
+            System.out.println("Saturday");
+            break;
+            default:
+            System.out.println("mai nahi hun chal ja");
+        }
+    }
+}
